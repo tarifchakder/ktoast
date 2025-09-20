6b967401b23c56302c8c6a9e19cea64e443493ec
tarif <tarifchakdar@gmail.com>
2025-09-20 20:39:15 +0400

    Remove platform-specific files
    This commit removes several platform-specific implementation files that are no longer needed.
    
    Files removed:
    - `composeApp/src/androidMain/kotlin/com/tarif/sample/Platform.android.kt`
    - `kmp-toast/src/androidMain/kotlin/io/github/tarifchakder/ktoast/Platform.android.kt`
    - `composeApp/src/iosMain/kotlin/com/tarif/sample/Platform.ios.kt`
    - `composeApp/src/desktopMain/kotlin/com/tarif/sample/Platform.jvm.kt`
    - `composeApp/src/wasmJsMain/kotlin/com/tarif/sample/Platform.wasmJs.kt`

--- composeApp/src/desktopMain/kotlin/com/tarif/sample/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.tarif.sample
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
